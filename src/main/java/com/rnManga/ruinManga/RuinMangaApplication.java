5b3fba4f54ebf0261073e2d7c198ca1a6c027bd9
tyrell-snyders <tyrell.snyders@gmail.com>
2023-10-01 20:35:56 +0200

    first commit

---
+++ src/main/java/com/rnManga/ruinManga/RuinMangaApplication.java
@@ -0,0 +1,13 @@
+package com.rnManga.ruinManga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuinMangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuinMangaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rnManga/ruinManga/RuinMangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rnManga.ruinManga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuinMangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
