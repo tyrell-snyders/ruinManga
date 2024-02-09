e184fd5c53c3a42dae44293faca978e5575a6eb7
tyrell-snyders <tyrell.snyders@gmail.com>
2024-02-09 21:51:40 +0200

    feat: get chapter pages

--- src/main/java/com/rnManga/ruinManga/services/impl/ChapterService.java
+++ src/main/java/com/rnManga/ruinManga/services/impl/ChapterService.java
@@ -2,18 +2,27 @@ package com.rnManga.ruinManga.services.impl;
 
 //imports
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.rnManga.ruinManga.middleware.PageHandling;
 import com.rnManga.ruinManga.services.iChapterService;
 
 @Service
 public class ChapterService implements iChapterService {
 
     private final String baseUrl = "https://api.mangadex.org/chapter/";
+    private final String chapUrl = "https://api.mangadex.org/at-home/server/";
     private final String mangaURL = "https://api.mangadex.org/manga";
     private static final Logger logger = LoggerFactory.getLogger(ComicService.class);
 
@@ -61,5 +70,19 @@ public class ChapterService implements iChapterService {
         return makeRequest(url, HttpMethod.GET, null);
     }
 
+    //Get the chapter pages
+    public Object getPagesNode(String id) {
+        String chapterUrl = chapUrl + id;
+        JsonNode chapterNode = makeRequest(chapterUrl, HttpMethod.GET, null);
+        if (chapterNode == null) return null;
 
+        JsonNode chapter = chapterNode.get("chapter");
+        if (chapter == null) return null;
+
+        PageHandling pageHandling = new PageHandling(chapter);
+
+        Object result = pageHandling.getChapters();
+
+        return result;
+    }
 }
